99ed0b03636cdb18bf9381f7cbd617fe77429bcc
Nate Ricklin <nate@rickl.in>
2017-12-19 16:01:42 -0700

    uncomment everything

--- task.py
+++ task.py
@@ -4,11 +4,11 @@ input_data = json.load(open('/mnt/work/input/ports.json'))
 
 # required params (will raise Exception if not there)
 destination = input_data['destination']  # should be of the form s3://bucket/prefix
-#access_key_id = input_data['access_key_id']  
-#secret_key = input_data['secret_key']  
+access_key_id = input_data['access_key_id']  
+secret_key = input_data['secret_key']  
 
 # Optional param.  Temp creds will contain a session token.
-#session_token = input_data.get('session_token')
+session_token = input_data.get('session_token')
 
 os.setenv('AWS_ACCESS_KEY_ID', access_key_id)
 os.setenv('AWS_SECRET_ACCESS_KEY',secret_key)
