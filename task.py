926152af4527a641790a8caf2070eb3efc74cee6
Nate Ricklin <nate@rickl.in>
2018-01-25 13:17:19 -0700

    make session token required

--- task.py
+++ task.py
@@ -6,14 +6,11 @@ input_data = json.load(open('/mnt/work/input/ports.json'))
 destination = input_data['destination']  # should be of the form s3://bucket/prefix
 access_key_id = input_data['access_key_id']  
 secret_key = input_data['secret_key']  
-
-# Optional param.  Temp creds will contain a session token.
-session_token = input_data.get('session_token')
+session_token = input_data['session_token']
 
 os.environ['AWS_ACCESS_KEY_ID'] = access_key_id
 os.environ['AWS_SECRET_ACCESS_KEY'] = secret_key
-if session_token:
-    os.environ['AWS_SESSION_TOKEN'] = session_token
+os.environ['AWS_SESSION_TOKEN'] = session_token
 
 cmd = "aws s3 cp /mnt/work/input/data %s --recursive" % destination
 
